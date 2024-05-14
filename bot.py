d3e4e8f1a2352d6749d93a0c31c243e7d1535951
nikitakuzmenko <nikita.kuzmenko08@gmail.com>
2024-05-14 20:50:58 +0300

    tts и README

--- bot.py
+++ bot.py
@@ -1,8 +1,10 @@
 import telebot
-
-from speechkit import voice_to_text
-from yandex_gpt import ask_gpt
-from config import TOKEN
+from speechkit import speech_to_text, text_to_speech
+from yandex import ask_gpt
+from config import TOKEN, max_tts_symbols, max_user_tts_symbols
+from db import count_all_symbol, insert_row
+MAX_USER_TTS_SYMBOLS = max_user_tts_symbols
+MAX_TTS_SYMBOLS = max_tts_symbols
 
 bot = telebot.TeleBot(TOKEN)
 
@@ -22,6 +24,58 @@ def log_errors(message):
     error_message = "Произошла ошибка с API GPT"
     bot.send_message(chat_id, error_message)
 
+@bot.message_handler(commands=['tts'])
+def tts_handler(message):
+    user_id = message.from_user.id
+    bot.send_message(user_id, 'Отправь следующим сообщеним текст, чтобы я его озвучил!')
+    bot.register_next_stap_handler(message, tts)
+
+def tts(message):
+    user_id = message.from_user.id
+    text = message.text
+
+    # Проверка, что сообщение действительно текстовое
+    if message.content_type != 'text':
+        bot.send_message(user_id, 'Отправь текстовое сообщение')
+        return
+
+        # Считаем символы в тексте и проверяем сумму потраченных символов
+    text_symbol = is_tts_symbol_limit(message, text)
+    if text_symbol is None:
+        return
+
+    # Записываем сообщение и кол-во символов в БД
+    insert_row(user_id, text, text_symbol)
+
+    # Получаем статус и содержимое ответа от SpeechKit
+    status, content = text_to_speech(text)
+
+    # Если статус True - отправляем голосовое сообщение, иначе - сообщение об ошибке
+    if status:
+        bot.send_voice(user_id, content)
+    else:
+        bot.send_message(user_id, content)
+
+def is_tts_symbol_limit(message, text):
+    user_id = message.from_user.id
+    text_symbols = len(text)
+
+    # Функция из БД для подсчёта всех потраченных пользователем символов
+    all_symbols = count_all_symbol(user_id) + text_symbols
+
+    # Сравниваем all_symbols с количеством доступных пользователю символов
+    if all_symbols >= MAX_USER_TTS_SYMBOLS:
+        msg = f"Превышен общий лимит SpeechKit TTS {MAX_USER_TTS_SYMBOLS}. Использовано: {all_symbols} символов. Доступно: {MAX_USER_TTS_SYMBOLS - all_symbols}"
+        bot.send_message(user_id, msg)
+        return None
+
+    # Сравниваем количество символов в тексте с максимальным количеством символов в тексте
+    if text_symbols >= MAX_TTS_SYMBOLS:
+        msg = f"Превышен лимит SpeechKit TTS на запрос {MAX_TTS_SYMBOLS}, в сообщении {text_symbols} символов"
+        bot.send_message(user_id, msg)
+        return None
+    return len(text)
+
 
 @bot.message.handler(content_types=['voice'])
 def handle_voice(message: telebot.types.Message):
@@ -40,7 +94,7 @@ def handle_voice(message: telebot.types.Message):
     file_id = message.voice.file_id
     file_info = bot.get_file(file_id)
     file = bot.download_file(file_info.file_path)
-    status, text = voice_to_text(file)
+    status, text = speech_to_text(file)
     if not status:
         bot.send_message(message.chat.id, text)
         return
--- speechkit.py
+++ speechkit.py
@@ -1,6 +1,31 @@
 import requests
 from config import IAM_TOKEN, FOLDER_ID
 
+def text_to_speech(text):
+    # iam_token, folder_id для доступа к Yandex SpeechKit
+    iam_token = IAM_TOKEN
+    folder_id = '<folder_id>'
+    # Аутентификация через IAM-токен
+    headers = {
+        'Authorization': f'Bearer {iam_token}',
+    }
+    data = {
+        'text': text,  # текст, который нужно преобразовать в голосовое сообщение
+        'lang': 'ru-RU',  # язык текста - русский
+        'voice': 'filipp',  # мужской голос Филиппа
+        'folderId': folder_id,
+    }
+    # Выполняем запрос
+    response = requests.post(
+        'https://tts.api.cloud.yandex.net/speech/v1/tts:synthesize',
+        headers=headers,
+        data=data
+    )
+    if response.status_code == 200:
+        return True, response.content  # возвращаем статус и аудио
+    else:
+        return False, "При запросе в SpeechKit возникла ошибка"
+
 
 def speech_to_text(data):
     params = "&".join([
